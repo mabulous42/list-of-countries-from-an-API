470910563da228a5ded402e00a8ab4dc0bc53634
Abbas <tundeeey@gmail.com>
2023-04-19 17:50:40 +0100

    added the showIndepent and showNonIndependent countries function and i was able to filter out the countries that are independent and those that are not, the result will display on the console on click of either of the button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let display = document.getElementById("display");
+
+
 async function displayCountries() {
     let allCountries = await fetch("https://restcountries.com/v3.1/all");
     let result = await allCountries.json();
@@ -18,3 +20,41 @@ async function displayCountries() {
 }
 
 displayCountries();
+
+async function showIndependent() {
+    let allCountries = await fetch("https://restcountries.com/v3.1/all");
+    let result = await allCountries.json();
+    let indptCountries = result.filter((country) => country.independent == true);
+    console.log(indptCountries);
+    indptCountries.forEach((element, index) => {
+        display.innerHTML += `
+         <tr class="">
+            <td>${index+1}</td>
+            <td>${element.name.common}</td>
+            <td>${element.capital}</td>
+            <td>${element.population}</td>
+            <td>${JSON.stringify(element.languages)}</td>
+            <td>${element.independent}</td>
+         </tr>
+         `
+    });
+}
+
+async function showNonIndependent() {
+    let allCountries = await fetch("https://restcountries.com/v3.1/all");
+    let result = await allCountries.json();
+    let nonIndptCountries = result.filter((country) => country.independent == false);
+    console.log(nonIndptCountries);
+    nonIndptCountries.forEach((element, index) => {
+        display.innerHTML += `
+         <tr class="">
+            <td>${index+1}</td>
+            <td>${element.name.common}</td>
+            <td>${element.capital}</td>
+            <td>${element.population}</td>
+            <td>${JSON.stringify(element.languages)}</td>
+            <td>${element.independent}</td>
+         </tr>
+         `
+    });
+}
